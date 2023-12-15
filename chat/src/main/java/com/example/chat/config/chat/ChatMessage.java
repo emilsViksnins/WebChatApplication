2bdf98411131ded43e8e626dcee97bebf6185f69
Emils Viksnins <viksnins.e@gmail.com>
2023-12-15 14:53:31 +0200

    Update

---
+++ chat/src/main/java/com/example/chat/config/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.chat.config.chat;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
